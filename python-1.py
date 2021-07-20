7b85068037abdd587047ee992694475d5b4062cc
Prajual Pillai <63894872+prajualpillai@users.noreply.github.com>
2021-07-20 15:19:08 +0530

    Update python-1.py

--- python-1.py
+++ python-1.py
@@ -1,12 +1,25 @@
-n = int(input())
-if 1<= n <=1000: 
-    for i in range(n):
-        q = list(map(int,input().rstrip().split())
-    
-        s = q[0]*q[1]
-        if s <= q[2]:
+def multiply(a,b,bound):
+    try:
+        s = a*b
+        if s <= bound:
             print(s)
         else:
-            print("multiplication of {} and {} with bound {} not possible".format(q[0],q[1],s))
-else:
+            print("multiplication of {} and {} with bound {} not possible".format(a,b,bound))
+    except ValueError:
+        print("Invalid Input")
+
+
+try:
+    n = int(input())
+    a = []
+    if 1<= n <=1000: 
+        for i in range(n):
+            q = list(map(int,input().rstrip().split()))
+            a.append(q)
+        for q in a:
+            multiply(q[0],q[1],q[2])
+            
+    else:
+        print("Invalid Input")
+except ValueError:
     print("Invalid Input")
